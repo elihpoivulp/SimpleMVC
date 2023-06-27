614b9ac674aaa5dc1b5fe19cd4e4041ec6160360
nicko <p.nickogamba@gmail.com>
2023-06-27 15:09:08 +0800

    Config file

---
+++ app/src/Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Simplemvc\Config;
+
+class Config
+{
+    public static string $defaultController = 'Home';
+
+    public static string $defaultActionMethod = 'index';
+
+    public static string $defaultNamespace = 'Simplemvc\\Controller';
+}
